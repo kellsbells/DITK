dd9613d264d55f770f45b45d761e816c272ba4db
kellsbells <kelleeballard@hotmail.com>
2015-09-15 06:14:52 -0600

    initial commit

---
+++ single-maindish.php
@@ -0,0 +1,13 @@
+<?php
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+			<?php get_template_part( 'content', 'maindish' ); ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
